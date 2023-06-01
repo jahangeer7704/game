d691082ed15bbeab96bbfb5b1361173229895690
Jahangeer.i <122361312+jahangeer7704@users.noreply.github.com>
2023-06-01 20:02:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# game
+# WEB_TECH
+Simple Stone paper scissor game using JS
+HTML Structure:
+
+Create an HTML file and add the basic structure with <head> and <body>.
+Inside the <body>, create containers for the game elements, such as the player's choice, computer's choice, and the result display area.
+CSS Styling:
+
+Apply CSS styles to the containers and elements to make them visually appealing.
+Use appropriate dimensions, colors, fonts, and positioning to create an attractive layout.
+Game Logic with JavaScript:
+
+Create JavaScript variables to store the choices (rock, paper, scissors) for the player and the computer.
+Implement event listeners to capture the player's choice when they click on the corresponding element.
+Generate a random choice for the computer using JavaScript's Math.random() function.
+Compare the player's choice with the computer's choice and determine the winner based on the rules of the game.
+Update the result display area to show the outcome (player wins, computer wins, or a tie).
+Updating the HTML:
+
+Use JavaScript to update the HTML elements dynamically based on the game's progress.
+Update the player's and computer's choice elements with their respective choices.
+Update the result display area to show the outcome of the game.
+Resetting the Game:
+
+Add a reset button to allow the player to start a new game.
+Implement a function that resets all game variables and clears the HTML elements to their initial state when the reset button is clicked.
+Optional Enhancements:
+
+Implement animations or transitions to enhance the user experience.
+Add sound effects when choices are made or when the game result is displayed.
+Include a score counter to keep track of the player's and computer's wins.
+Remember, this is just an overview of the steps involved in creating a rock-paper-scissors game using HTML, CSS, and JavaScript. You will need to write the actual code to implement each step. You can refer to online resources or tutorials to find specific code examples and further guidance on each step.
+
+
+
+
---
+++ game.js
@@ -0,0 +1,80 @@
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    responsiveVoice.speak("Welcome to the game!")
+ 
+    init();
+  }
+});
+
+let paper = document.querySelector("#paper");
+let rock = document.querySelector("#rock");
+let scissor = document.querySelector("#scissor");
+
+const init = () => {
+  const rockf =() => {
+    uv("rock");
+  };
+  const paperf = () => {
+    uv("paper");
+  };
+  const scissorf = () => {
+    uv("scissor");
+  };
+  const uv = (input) => {
+    userdec(input);
+  };
+  const userdec = (inp) => {
+    let com = comdec();
+    merge(inp, com);
+    removeClickEvents();
+  };
+  const comdec = () => {
+    const ran = Math.floor(Math.random() * 3 + 1);
+    
+    const arr = ["rock", "paper", "scissor"];
+    return arr[ran - 1];
+  };
+  const merge = (user, com) => {
+    const pl = user;
+    const pc = com;
+    const cw = "Computer won";
+    const pw = "you won";
+    let re =
+      pl === pc
+        ? "tie"
+        : pl === "rock" && pc === "paper"
+        ? cw
+        : pl === "rock" && pc === "scissor"
+        ? pw
+        : pl === "paper" && pc == "rock"
+        ? pw
+        : pl === "paper" && pc === "scissor"
+        ? cw
+        : pl === "scissor" && pc === "rock"
+        ? cw
+        : pl === "scissor" && pc === "paper"
+        ? pw
+        : "invalid";
+        finalResult(re, pc, pl);
+      };
+      const finalResult = (r, m, p) => {
+        let res = document.getElementById("res");
+        res.innerHTML = "Computer Choosed " + m + ", so " + r;
+        let rest = "Computer Choosed " + m + ", so " + r;
+        responsiveVoice.speak(rest)
+        responsiveVoice.speak("click replay,to play again")
+      };
+      const removeClickEvents = () => {
+        paper.removeEventListener("click", paperf);
+        rock.removeEventListener("click", rockf);
+        scissor.removeEventListener("click", scissorf);
+      };
+      rock.addEventListener("click", rockf);
+      paper.addEventListener("click", paperf);
+      scissor.addEventListener("click", scissorf);
+    };
+    
+    const retry = () => {
+  location.reload();
+  responsiveVoice.speak("Replayed,click the choice");
+};
